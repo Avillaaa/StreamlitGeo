57badc2ec247b5eb9b9b30ad4a2f49bf194303c8
Avillaaa <avillaalif13@gmail.com>
2025-04-02 12:07:38 +0700

    adad

--- pages/4_Heatmap.py
+++ pages/4_Heatmap.py
@@ -16,18 +16,30 @@ A Streamlit map template
 """
 
 st.title("Heatmap")
-
-
 filepath = "temp_geocoding.csv"  # Ganti dengan path file CSV Anda
-m = leafmap.Map(center=[-7, 108], zoom=5)
-regions = "prov 37.geojson"
-m.add_geojson(regions, layer_name="Provinsi Indonesia")
-m.add_heatmap(
-    filepath,
-    latitude="Latitude",
-    longitude="Longitude",
-    value="value",
-    name="Heat map",
-    radius=20,
-)
-m.to_streamlit(height=700)
+
+
+col1, col2 = st.columns([4, 1])
+options = list(leafmap.basemaps.keys())
+index = options.index("OpenTopoMap")
+
+with col2:
+
+    basemap = st.selectbox("Pilih basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(center=[-7, 108], zoom=5)
+    regions = "prov 37.geojson"
+    m.add_geojson(regions, layer_name="Provinsi Indonesia")
+    m.add_heatmap(
+        filepath,
+        latitude="Latitude",
+        longitude="Longitude",
+        value="value",
+        name="Heat map",
+        radius=20,
+    )
+    m.to_streamlit(height=700)
+
