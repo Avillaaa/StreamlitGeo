743cdd213c093961cc4deb4c5c8257bc2033ba6c
Avillaaa <avillaalif13@gmail.com>
2025-04-03 10:27:37 +0700

    EFEF

--- pages/3_Cluster_Penyebaran.py
+++ pages/3_Cluster_Penyebaran.py
@@ -16,14 +16,18 @@ col1, col2 = st.columns([4, 1])
 options = list(leafmap.basemaps.keys())
 index = options.index("OpenStreetMap")
 
-cities = "./geocoding.csv"
-regions = "prov 37.geojson"
-df = pd.read_csv(cities)
-
-
 with col2:
 
     basemap = st.selectbox("Pilih basemap:", options, index)
+
+
+with col1:
+
+    m = leafmap.Map(center=[40, -100], zoom=4)
+    cities = "./geocoding.csv"
+    regions = "prov 37.geojson"
+    df = pd.read_csv(cities)
+
     search_query = st.text_input("Cari berdasarkan NO RM:")
     if search_query:
         filtered_df = df[df["NO RM"].astype(str).str.contains(search_query, case=False, na=False)]
@@ -33,16 +37,12 @@ with col2:
             st.success(f"Ditemukan {len(filtered_df)} hasil yang cocok.")
     else:
         filtered_df = df
+
     # Menambahkan dropdown filter
     jenisbpjs = filtered_df["JENIS BPJS"].unique()  # Ganti "JENIS BPJS" dengan nama kolom yang ingin difilter
     selected_region = st.multiselect("Pilih JENIS BPJS:", jenisbpjs)
 
     filtered_df = filtered_df[filtered_df["JENIS BPJS"] == selected_region]
-
-
-with col1:
-
-    m = leafmap.Map(center=[40, -100], zoom=4)
 
     m.add_geojson(regions, layer_name="Provinsi Indonesia")
     m.add_points_from_xy(
